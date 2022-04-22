b467483ecc9fa7c02a791f8852f10e0bae41fc0c
Vanessa Dias <v.diasn1@gmail.com>
2022-04-22 18:42:13 -0300

    atualização de exercícios

---
+++ aula12ex/ex015/script.js
@@ -0,0 +1,44 @@
+function verificar() {
+  var data = new Date()
+  var ano = data.getFullYear()
+  var fano = document.getElementById('txtano')
+  var res = document.getElementById('res')
+  if (fano.value.length == 0 || Number(fano.value) > ano) {
+    window.alert('[ERRO] Verifique os dados e tente novamente!')
+  } else {
+    var fsex = document.getElementsByName('radsex')
+    var idade = ano - Number(fano.value)
+    var gênero = ''
+    var img = document.createElement('img')
+    img.setAttribute('id', 'foto')
+    if (fsex[0].checked) {
+      gênero = 'Homem'
+      if (idade >=0 && idade < 10) {
+          //Criança
+          img.setAttribute('src', 'h-crianca.png')
+      } else if (idade < 21) {
+          //Adolescente
+          img.setAttribute('src', 'h-adolescente.png')
+      } else if (idade < 50) {
+          //Adulto
+          img.setAttribute('src', 'h-adulto.png')
+      } else {
+          //Idoso
+          img.setAttribute('src', 'h-idoso.png')
+      }
+    } else if (fsex[1].checked) {
+      gênero = 'Mulher'
+      if (idade >=0 && idade < 10) {
+        //Criança
+    } else if (idade < 21) {
+        //Adolescente
+    } else if (idade < 50) {
+        //Adulto
+    } else {
+        //Idoso
+    }
+    }
+    res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+    res.appendChild(img)
+}
+}
